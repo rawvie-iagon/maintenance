74837b131db55284f745f116be01b35e0a205a4a
rawvie <rawvie@ngit.com.np>
2024-09-01 21:36:00 +0545

    add countdown

--- src/App.js
+++ src/App.js
@@ -1,4 +1,49 @@
+import Countdown from "react-countdown";
+
+function pad(d) {
+  return d < 10 ? "0" + d.toString() : d.toString();
+}
+
+const renderer = ({ hours, minutes, seconds, completed }) => {
+  if (completed) {
+    return (
+      <h3
+        style={{
+          textAlign: "center",
+          fontFamily: "Karla, sans-serif",
+          margin: "15px",
+        }}
+      >
+        App will be live in no time! Please be patient.
+      </h3>
+    );
+  } else {
+    return (
+      <div
+        style={{
+          margin: "15px",
+          display: "flex",
+          flexDirection: "column",
+          gap: "5px",
+          alignItems: 'center'
+        }}
+      >
+        <h3 style={{ margin: 0, padding: 0 }}>
+          Currently down for maintenance, expected to be back in:
+        </h3>
+        <h1 style={{ margin: 0, padding: 0 }}>
+          {pad(Number(hours))}:{pad(Number(minutes))}:{pad(seconds)}
+        </h1>
+      </div>
+    );
+  }
+};
+
 function App() {
+  const epochStartTime = new Date("2024-09-01T21:44:51.00Z");
+  const expectedEndTime = new Date(
+    epochStartTime.getTime() + 15 * 60 * 1000
+  );
   return (
     <div
       style={{
@@ -16,15 +61,7 @@ function App() {
         style={{ height: "70px" }}
         src="https://docs.iagon.com/img/iagon-logo-dark.png"
       />
-      <h3
-        style={{
-          textAlign: "center",
-          fontFamily: "Karla, sans-serif",
-          margin: "15px",
-        }}
-      >
-        Down for maintenance, will be back up soon.
-      </h3>
+      <Countdown date={expectedEndTime} renderer={renderer} />
     </div>
   );
 }
